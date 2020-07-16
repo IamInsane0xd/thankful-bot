71b8f0224ee06809c38573223569f978c8b6461f
IamInsane0xd <62342792+IamInsane0xd@users.noreply.github.com>
2020-07-16 14:05:50 +0200

    Create sgst.py

---
+++ cogs/sgst.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+class Sgst(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def suggest(self, ctx, type = None, *, sgst = None):
+        try:
+            await ctx.channel.purge(limit = 1)
+        except discord.Forbidden:
+            print("Cant delete message >:c")
+            print('')
+
+        if type == "server" or type == "bot":
+            if sgst != None
+                if type == 'server':
+                    channel = get(ctx.message.guild.channels, name = 'server-suggestions')
+
+                    await channel.send('New **server-suggestion** from *{}*:\n```{}```'.format(str(ctx.message.author)[:-5], sgst))
+
+                if type == 'bot':
+                    channel = get(ctx.message.guild.channels, name = 'bot-suggestions')
+
+                    await channel.send('New **server-suggestion** from *{}*:\n```{}```'.format(str(ctx.message.author)[:-5], sgst))
+
+            else:
+                await ctx.send('Please use the correct format: `t.suggest <server|bot> <suggestion>`')
+
+        else:
+            await ctx.send('Please use the correct format: `t.suggest <server|bot> <suggestion>`')
+
+def setup(client):
+    client.add_cog(Sgst(client))
